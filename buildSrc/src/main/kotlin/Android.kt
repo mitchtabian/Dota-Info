7d30eea8657554c1f2a3ea4eb08bdb574cecd3f6
mitch <mitch@tabian.ca>
2021-07-28 13:30:33 -0700

    cleanup deps

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,9 +1,9 @@
 object Android {
-    val appId = "com.codingwithmitch.dotainfo"
-    val compileSdk = 30
-    val buildTools = "30.0.3"
-    val minSdk = 21
-    val targetSdk = 30
-    val versionCode = 1
-    val versionName = "1.0"
+    const val appId = "com.codingwithmitch.dotainfo"
+    const val compileSdk = 30
+    const val buildTools = "30.0.3"
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
 }
--- buildSrc/src/main/kotlin/Compose.kt
+++ buildSrc/src/main/kotlin/Compose.kt
@@ -2,7 +2,7 @@ object Compose {
     private const val activityComposeVersion = "1.3.0-rc01"
     const val activity = "androidx.activity:activity-compose:$activityComposeVersion"
 
-    const val composeVersion = "1.0.0-rc01"
+    const val composeVersion = "1.0.0"
     const val ui = "androidx.compose.ui:ui:$composeVersion"
     const val material = "androidx.compose.material:material:$composeVersion"
     const val tooling = "androidx.compose.ui:ui-tooling:$composeVersion"
@@ -12,9 +12,6 @@ object Compose {
 
     private const val hiltNavigationComposeVersion = "1.0.0-alpha03"
     const val hiltNavigation = "androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion"
-
-    private const val constraintLayoutVersion = "1.0.0-alpha08"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:$constraintLayoutVersion"
 }
 
 object ComposeTest {
