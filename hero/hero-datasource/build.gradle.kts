3770059ee9f3e40df8bd4f52f1e3a4c6dc2b386e
mitch <mitch@tabian.ca>
2021-07-29 11:50:30 -0700

    HeroService with ktor

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -2,6 +2,14 @@ apply {
     from("$rootDir/library-build.gradle")
 }
 
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
 
 dependencies {
+    "implementation"(project(Modules.heroDomain))
+
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 }
---
+++ hero/hero-datasource/src/main/java/com/codingwithmitch/hero_datasource/network/EndPoints.kt
@@ -0,0 +1,6 @@
+package com.codingwithmitch.hero_datasource.network
+
+object EndPoints {
+    const val BASE_URL = "https://api.opendota.com"
+    const val HERO_STATS = "$BASE_URL/api/heroStats"
+}
