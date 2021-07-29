4f17c77d24f91308f49594e149c9413f01313ab5
mitch <mitch@tabian.ca>
2021-07-29 11:11:16 -0700

    Build Hero.kt

---
+++ hero/hero-domain/src/main/java/com/codingwithmitch/hero_domain/HeroAttribute.kt
@@ -0,0 +1,90 @@
+package com.codingwithmitch.hero_domain
+
+sealed class HeroAttribute(
+    val uiValue: String,
+    val abbreviation: String,
+){
+    object Agility: HeroAttribute(
+        uiValue = "Agility",
+        abbreviation = "agi",
+    )
+
+   object Strength: HeroAttribute(
+       uiValue = "Strength",
+       abbreviation = "str",
+   )
+
+    object Intelligence: HeroAttribute(
+        uiValue = "Intelligence",
+        abbreviation = "int",
+    )
+
+    object Unknown: HeroAttribute(
+        uiValue = "Unknown",
+        abbreviation = "unknown",
+    )
+}
+
+fun getHeroAttrFromUiValue(uiValue: String): HeroAttribute{
+    return when(uiValue){
+        HeroAttribute.Agility.uiValue -> {
+            HeroAttribute.Agility
+        }
+        HeroAttribute.Strength.uiValue -> {
+            HeroAttribute.Strength
+        }
+        HeroAttribute.Intelligence.uiValue -> {
+            HeroAttribute.Intelligence
+        }
+        else -> HeroAttribute.Unknown
+    }
+}
+
+fun getHeroAttrFromAbbreviation(abbreviation: String): HeroAttribute{
+    return when(abbreviation){
+        HeroAttribute.Agility.abbreviation -> {
+            HeroAttribute.Agility
+        }
+        HeroAttribute.Strength.abbreviation -> {
+            HeroAttribute.Strength
+        }
+        HeroAttribute.Intelligence.abbreviation -> {
+            HeroAttribute.Intelligence
+        }
+        else -> HeroAttribute.Unknown
+    }
+}
+
+fun Hero.minAttackDmg(): Int {
+    return when(primaryAttribute){
+        is HeroAttribute.Strength -> {
+            baseAttackMin + baseStr
+        }
+        is HeroAttribute.Agility -> {
+            baseAttackMin + baseAgi
+        }
+        is HeroAttribute.Intelligence -> {
+            baseAttackMin + baseInt
+        }
+        is HeroAttribute.Unknown -> {
+            0
+        }
+    }
+}
+
+fun Hero.maxAttackDmg(): Int {
+    return when(primaryAttribute){
+        is HeroAttribute.Strength -> {
+            baseAttackMax + baseStr
+        }
+        is HeroAttribute.Agility -> {
+            baseAttackMax + baseAgi
+        }
+        is HeroAttribute.Intelligence -> {
+            baseAttackMax + baseInt
+        }
+        is HeroAttribute.Unknown -> {
+            0
+        }
+    }
+}
--- hero/hero-domain/src/main/java/com/codingwithmitch/hero_domain/Placeholder.kt
+++
@@ -1,4 +0,0 @@
-package com.codingwithmitch.hero_domain
-
-class Placeholder {
-}
