ba6c73b2c9c401bbeab4f567eb235e1b13d0d12f
mitch <mitch@tabian.ca>
2021-08-03 12:57:40 -0700

    fix build.gradle.kts

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -1,7 +1,14 @@
 apply {
     from("$rootDir/library-build.gradle")
 }
-
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
 
 dependencies {
+    "implementation"(project(Modules.heroDomain))
+
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 }
