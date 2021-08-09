48f344dcc9e91ce1a97c50a87507f71b21e98694
mitch <mitch@tabian.ca>
2021-08-09 09:36:38 -0700

    core data structures for filtering and ordering

--- hero/ui-heroList/src/main/java/com/codingwithmitch/ui_herolist/ui/HeroListState.kt
+++ hero/ui-heroList/src/main/java/com/codingwithmitch/ui_herolist/ui/HeroListState.kt
@@ -1,11 +1,16 @@
 package com.codingwithmitch.ui_herolist.ui
 
+import com.codingwithmitch.core.domain.FilterOrder
 import com.codingwithmitch.core.domain.ProgressBarState
 import com.codingwithmitch.hero_domain.Hero
+import com.codingwithmitch.hero_domain.HeroAttribute
+import com.codingwithmitch.hero_domain.HeroFilter
 
 data class HeroListState(
     val progressBarState: ProgressBarState = ProgressBarState.Idle,
     val heros: List<Hero> = listOf(),
     val filteredHeros: List<Hero> = listOf(),
     val heroName: String = "",
+    val heroFilter: HeroFilter = HeroFilter.Hero(FilterOrder.Descending),
+    val primaryAttrFilter: HeroAttribute = HeroAttribute.Unknown,
 )
