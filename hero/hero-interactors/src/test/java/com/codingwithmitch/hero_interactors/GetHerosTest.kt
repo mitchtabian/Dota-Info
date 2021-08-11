58f7a9db6b134bd6ef82e7774bedc039d09c8512
mitch <mitch@tabian.ca>
2021-08-11 10:13:11 -0700

    GetHeros use-case malformed data test (failure)

--- hero/hero-interactors/src/test/java/com/codingwithmitch/hero_interactors/GetHerosTest.kt
+++ hero/hero-interactors/src/test/java/com/codingwithmitch/hero_interactors/GetHerosTest.kt
@@ -146,6 +146,46 @@ class GetHerosTest {
         assert(emissions[2] == DataState.Loading<List<Hero>>(ProgressBarState.Idle))
     }
 
+    @Test
+    fun getHeros_malformedData() =  runBlocking {
+        // setup
+        val heroDatabase = HeroDatabaseFake()
+        val heroCache = HeroCacheFake(heroDatabase)
+        val heroService = HeroServiceFake.build(
+            type = HeroServiceResponseType.MalformedData // Malformed data
+        )
+
+        getHeros = GetHeros(
+            cache = heroCache,
+            service = heroService
+        )
+
+        // Confirm the cache is empty before any use-cases have been executed
+        var cachedHeros = heroCache.selectAll()
+        assert(cachedHeros.isEmpty())
+
+        // Execute the use-case
+        val emissions = getHeros.execute().toList()
+
+        // First emission should be loading
+        assert(emissions[0] == DataState.Loading<List<Hero>>(ProgressBarState.Loading))
+
+        // Confirm second emission is error response
+        assert(emissions[1] is DataState.Response)
+        assert(((emissions[1] as DataState.Response).uiComponent as UIComponent.Dialog).title == "Network Data Error")
+        assert(((emissions[1] as DataState.Response).uiComponent as UIComponent.Dialog).description.contains("Unexpected JSON token at offset"))
+
+        // Confirm third emission is empty list of data
+        assert(emissions[2] is DataState.Data)
+        assert((emissions[2] as DataState.Data).data?.size == 0)
+
+        // Confirm the cache is STILL EMPTY
+        cachedHeros = heroCache.selectAll()
+        assert(cachedHeros.isEmpty())
+
+        // Confirm loading state is IDLE
+        assert(emissions[3] == DataState.Loading<List<Hero>>(ProgressBarState.Idle))
+    }
 }
 
 
