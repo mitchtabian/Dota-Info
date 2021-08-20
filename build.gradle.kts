cab3c148c222403552c0b73b446e7728d3615e7b
Xavier Rubio Jansana <xrubioj@users.noreply.github.com>
2021-08-21 00:05:17 +0200

    Revert "Adds bye-bye-jetifier Gradle plugin"
    This reverts commit 042d3abc0adfc8085a2fb8ae18809ca4a71d87ff.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,8 @@ buildscript {
         classpath(Build.kotlinGradlePlugin)
         classpath(Build.hiltAndroid)
         classpath(Build.sqlDelightGradlePlugin)
-        classpath(Build.byeByeJetifier)
     }
 }
-
-apply(plugin = "com.dipien.byebyejetifier")
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
