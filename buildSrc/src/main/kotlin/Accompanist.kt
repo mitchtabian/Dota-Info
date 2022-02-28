6547e84faaef1f492b819d38adfbc649ac4ba551
Rafael Wolf de Goes <rafael.wolfdegoes@affinitycu.ca>
2022-02-28 11:26:05 -0600

    Update Accompanist

--- buildSrc/src/main/kotlin/Accompanist.kt
+++ buildSrc/src/main/kotlin/Accompanist.kt
@@ -1,4 +1,4 @@
 object Accompanist {
-    private const val animationsVersion = "0.23.0"
+    private const val animationsVersion = "0.23.1"
     const val animations = "com.google.accompanist:accompanist-navigation-animation:$animationsVersion"
 }
